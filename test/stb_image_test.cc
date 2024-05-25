c2bf41df16c2558fad4d07167a0721e6670477c7
hangdada <824362330@qq.com>
2024-05-25 14:39:13 +0800

    [feature] 1、引入第三方图片解码库stb_image，并为其创建接口库 2、添加assets资产文件夹，目前只有texture 3、针对资产文件夹方便使用，编译成了接口库，方便通过一个宏获取资产文件夹路径
    [test]
    1、给stb_image添加测试文件
    
    [chore]
    1、添加

---
+++ test/stb_image_test.cc
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include <string>
+
+
+#include "stb_image.h"
+
+
+
+int Pass()
+{
+    std::cout << "thirdparts library stb_image Pass" << std::endl;
+    return 0;
+}
+
+int FailPass()
+{
+     std::cout << "thirdparts library stb_image Can't Pass" << std::endl;
+    return -1;
+}
+
+
+int main(int argc, char** argv)
+{
+
+    int width{ 0 }, height{ 0 };
+    int picType { 0 };
+
+    std::string path = ASSETS_PATH;
+    path += "/texture/lufei.jpg";
+	//由于我们是BGRA的格式，图片是RGBA的格式，所以得交换下R&B
+	unsigned char* bits = stbi_load(path.c_str(), &width, &height, &picType, STBI_rgb_alpha);
+
+    if(nullptr == bits)
+    {
+        return FailPass();
+    }
+    
+    stbi_image_free(bits);
+
+    return Pass();
+}
